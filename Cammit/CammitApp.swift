b52162849625a8662a5d4dd5331fe5646fcabb9f
Bhavadarshini <bhavadarshini05@gmail.com>
2025-06-22 22:44:53 +0530

    Initial Commit

---
+++ Cammit/CammitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CammitApp.swift
+//  Cammit
+//
+//  Created by Bhavadarshini on 22/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CammitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
